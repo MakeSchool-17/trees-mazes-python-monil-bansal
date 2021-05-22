cd5ed2d521a10d270f62a9082b1321fb45c61d70
monil-bansal <31930832+monil-bansal@users.noreply.github.com>
2021-05-23 00:16:11 +0530

    Update solve_maze.py

--- solve_maze.py
+++ solve_maze.py
@@ -1,12 +1,30 @@
 import maze
 import generate_maze
 import sys
+import random
 
+def do_dfs(m, current_cell, goal_cell):
+    m.refresh_maze_view()
+    if current_cell == goal_cell:
+        return True
+    neighbors = m.cell_neighbors(current_cell)
+   
+    while(len(neighbors)):
+        sz = len(neighbors)
+        idx = random.randint(0,sz-1)
+        neighbor, compass_idx = neighbors[idx]
+        m.visit_cell(current_cell,neighbor,compass_idx)
+        if(do_dfs(m,neighbor,goal_cell)):
+            return True
+        neighbors = m.cell_neighbors(current_cell)
+
+    m.backtrack(current_cell)
 
 # Solve maze using Pre-Order DFS algorithm, terminate with solution
 def solve_dfs(m):
-    # TODO: Implement solve_dfs
-    pass
+    current_cell = 0
+    goal_cell = m.total_cells - 1
+    do_dfs(m,current_cell,goal_cell)
 
 
 # Solve maze using BFS algorithm, terminate with solution
